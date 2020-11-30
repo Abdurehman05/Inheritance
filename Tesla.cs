c784bc50b4f871eb09915ff8261aaef297f7e324
Abdu <abdukr5@gmail.com.com>
2020-11-30 10:44:56 -0600

    first commit

---
+++ Tesla.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace Inheritance
+{
+    public class Tesla : Vehicle
+    {
+        public double BatteryKWh { get; set; }
+
+        public void ChargeBattery()
+        {
+
+        }
+    }
+}
